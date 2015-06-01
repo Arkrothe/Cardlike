ca2e7fdf130e51325ec2135a9729dd344bf2c681
Arkrothe <kapiljain.128@gmail.com>
2015-06-02 01:29:50 +0530

    First commit Yay\!

---
+++ Assets/Scripts/CardBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardBehaviour : MonoBehaviour {
+    public int health;
+    public int attack;
+    public int speed;
+    private int currentHealth;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
